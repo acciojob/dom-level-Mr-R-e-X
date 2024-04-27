5921b9801696a6479c7af635457570370006f911
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-27 12:54:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let depth = 0;
+let list = document.querySelector("ul");
+// console.log(list.children);
+for (let i = 0; i < list.children.length; i++) {
+  if (list.children[i].getAttribute("id") === "level") {
+    depth = i;
+    break;
+  }
+  depth = i;
+}
+window.alert(depth);
